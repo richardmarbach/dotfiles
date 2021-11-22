3708ece0006e8ac352d4528510c7b9a413c493aa
Richard Marbach <rmarbach@gmail.com>
2021-11-22 08:36:05 +0100

    Telescope shows on bottom

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -4,12 +4,15 @@ function M.setup()
   local telescope = require('telescope')
 
   telescope.setup {
+    defaults = {
+      layout_strategy = 'bottom_pane',
+    },
     extensions = {
       fzf = {
         override_generic_sorter = false,
         override_file_sorter = true,
       }
-    }
+    },
   }
 
   telescope.load_extension('fzf')
