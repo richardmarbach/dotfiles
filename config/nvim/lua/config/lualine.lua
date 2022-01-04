369b5ab4bd6ac21c7bf43c631101a20e7a54fd1e
Richard Marbach <rmarbach@gmail.com>
2022-01-04 08:30:47 +0100

    Statusline shows full file path

--- config/nvim/lua/config/lualine.lua
+++ config/nvim/lua/config/lualine.lua
@@ -4,6 +4,22 @@ function M.setup()
   require('lualine').setup {
     options = {
       icons_enabled = false
+    },
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          path = 1,
+        }
+      }
+    },
+    inactive_sections = {
+      lualine_c = {
+        {
+          'filename',
+          path = 1,
+        }
+      }
     }
   }
 end
