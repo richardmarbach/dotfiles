02664e6a801252be177d45bdc7efabe16a0e8a1f
Richard Marbach <rmarbach@gmail.com>
2022-01-15 10:50:51 +0100

    Use middleware pattern for configuring lsp settings

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -4,9 +4,33 @@ local function get_capabilities()
 	return require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 end
 
+local function on_attach(_, bufnr)
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	require("keymaps").setup_lsp_mappings(bufnr)
+end
+
+local function disable_format(next)
+	return function(client, bufnr)
+		next(client, bufnr)
+		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
+	end
+end
+
+local function format_on_save(next)
+	return function(client, bufnr)
+		next(client, bufnr)
+		vim.api.nvim_command([[augroup Format]])
+		vim.api.nvim_command([[autocmd! * <buffer>]])
+		vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]])
+		vim.api.nvim_command([[augroup END]])
+	end
+end
+
 local lsp_configs = {
 	["sumneko_lua"] = require("lua-dev").setup(),
 	["solargraph"] = {
+		on_attach = disable_format(on_attach),
 		init_options = {
 			formatting = false,
 		},
@@ -16,25 +40,23 @@ local lsp_configs = {
 			},
 		},
 	},
+	["rust_analyzer"] = {
+		on_attach = format_on_save(on_attach),
+	},
 }
 
 local function setup_lsp_config(provider)
 	local config = {
-		on_attach = function(client, bufnr)
-			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-			require("keymaps").setup_lsp_mappings(bufnr)
-			client.resolved_capabilities.document_formatting = false
-			client.resolved_capabilities.document_range_formatting = false
-		end,
+		on_attach = on_attach,
 		flags = {
 			debounce_text_changes = 50,
 		},
 		capabilities = get_capabilities(),
 	}
 
-  if lsp_configs[provider] then
+	if lsp_configs[provider] then
 		config = vim.tbl_extend("force", config, lsp_configs[provider])
-  end
+	end
 
 	return config
 end
@@ -51,10 +73,6 @@ function M.setup()
 			local keymaps = require("keymaps")
 			if client.resolved_capabilities.document_formatting then
 				keymaps.set_buf_keymap(bufnr, "n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>")
-				--    vim.api.nvim_command[[augroup Format]]
-				--    vim.api.nvim_command[[autocmd! * <buffer>]]
-				--    vim.api.nvim_command[[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-				--    vim.api.nvim_command[[augroup END]]
 			end
 			if client.resolved_capabilities.document_range_formatting then
 				keymaps.set_buf_keymap(bufnr, "n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>")
