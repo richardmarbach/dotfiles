04b077bcb268e883d414d271633e83c502b622ff
Richard Marbach <rmarbach@gmail.com>
2022-07-07 11:41:50 +0200

    Fix ruby ft

--- config/nvim/after/ftplugin/ruby.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("o", "<leader>yc" require('extract-ruby-constant').extract(), { noremap = true })
-vim.keymap.set("i", "<leader>yc" require('extract-ruby-constant').yank(), { noremap = true })
---
+++ config/nvim/after/ftplugin/ruby.vim
@@ -0,0 +1,2 @@
+onoremap <leader>yc <Cmd>lua require("extract-ruby-constant").extract()<cr>
+inoremap <leader>yc <Cmd>lua require("extract-ruby-constant").yank()<cr>
