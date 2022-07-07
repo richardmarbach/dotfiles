329d5eefc696445501582293a06fbd693a157011
Richard Marbach <rmarbach@gmail.com>
2022-07-07 09:25:16 +0200

    Formatting

--- config/nvim/lua/colorscheme.lua
+++ config/nvim/lua/colorscheme.lua
@@ -1,7 +1,7 @@
 -- 24 bit colors
 vim.o.termguicolors = true
 
-vim.o.background = 'dark'
+vim.o.background = "dark"
 vim.g.gruvbox_filetype_hi_groups = 1
 vim.g.gruvbox_plugin_hi_groups = 1
-vim.cmd('colorscheme gruvbox8_soft')
+vim.cmd("colorscheme gruvbox8_soft")
