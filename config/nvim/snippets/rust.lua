a522c12a3e6d5ca3bb194dbc27d9dbde4ad723be
Richard Marbach <rmarbach@gmail.com>
2022-07-13 17:12:55 +0200

    Rust snippets

--- config/nvim/snippets/rust.lua
+++ config/nvim/snippets/rust.lua
@@ -1,19 +1,39 @@
 --- @diagnostic disable: undefined-global
 
-return {
-  s("struct", {
-    t("struct "),
-    i(1, "Struct"),
-    t({ " {", "\t" }),
-    i(2),
-    t({ "", "}" }),
-  }),
-
-  s("#derive", {
+local function derive()
+  return sn(nil, {
     t("#[derive("),
     i(1, "Debug"),
     t(")]"),
+  })
+end
+
+return {
+  s("struct", {
+    c(2, {
+      derive(),
+      t(""),
+    }),
+    t({ "", "struct " }),
+    i(1, "Struct"),
+    t({ " {", "\t" }),
+    i(3),
+    t({ "", "}" }),
   }),
+
+  s("impl", {
+    t("impl "),
+    i(1),
+    c(2, {
+      t(""),
+      sn(nil, { t(" for "), i(1) }),
+    }),
+    t({ " {", "\t" }),
+    i(3),
+    t({ "", "}" }),
+  }),
+
+  s("#derive", derive()),
 
   s("#", {
     t("#["),
@@ -23,6 +43,28 @@ return {
     t(")]"),
   }),
 
+  s(
+    "method",
+    fmt(
+      [[
+      fn(<>, <>) <> {
+        <>
+      }
+    ]] ,
+      {
+        c(1, {
+          t("&self"),
+          t("&mut self"),
+          t("self"),
+        }),
+        i(2),
+        i(3),
+        i(4),
+      },
+      { delimiters = "<>" }
+    )
+  ),
+
   s("fn", {
     t("fn "),
     i(1),
@@ -30,7 +72,7 @@ return {
     i(2),
     t(")"),
     i(3),
-    t({ "{", "\t" }),
+    t({ " {", "\t" }),
     i(4),
     t({ "", "}" }),
   }),
@@ -61,16 +103,6 @@ return {
     t(";"),
   }),
 
-  s("for", {
-    t("for "),
-    i(1),
-    t(" in "),
-    i(2),
-    t({ " {", "\t" }),
-    i(3),
-    t({ "", "}" }),
-  }),
-
   s("println", {
     t('println!("'),
     c(1, {
@@ -91,6 +123,26 @@ return {
     i(3),
     t({ "", "};" }),
   }),
+
+  s(
+    "for",
+    fmt(
+      [[
+      for {} in {} {{
+        {}
+      }}
+    ]] ,
+      {
+        i(1, "i"),
+        c(2, {
+          i(nil),
+          sn(nil, { r(1, "start"), t(".."), r(2, "end") }),
+          sn(nil, { r(1, "start"), t("..="), r(2, "end") }),
+        }),
+        i(3),
+      }
+    )
+  ),
 
   s("type", {
     t("type "),
