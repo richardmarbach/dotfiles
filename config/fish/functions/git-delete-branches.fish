1190851238debca065b357d9aa776d7df32274a0
Richard Marbach <rmarbach@gmail.com>
2022-12-08 12:10:11 +0100

    Exclude main from git-delete-branches

--- config/fish/functions/git-delete-branches.fish
+++ config/fish/functions/git-delete-branches.fish
@@ -1,7 +1,7 @@
 function git-delete-branches
   git branch |
     grep --invert-match '\*' |
-    grep --invert-match 'master' |
+    grep --invert-match 'master|main' |
     cut -c 3- |
     fzf --multi --preview="git l {} --" |
     gxargs --no-run-if-empty git branch --delete --force
