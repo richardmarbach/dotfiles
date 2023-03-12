2fdf28045931b049a832735b5cd042b51113e486
Richard Marbach <rmarbach@gmail.com>
2023-03-12 08:17:15 +0100

    Update harpoon jumps

--- config/nvim/lua/plugins/tools.lua
+++ config/nvim/lua/plugins/tools.lua
@@ -120,12 +120,12 @@ return {
     keys = {
       { "<leader>ha", function() require("harpoon.mark").add_file() end, { desc = "[H]arpoon [A]dd" } },
       { "<leader>hv", function() require("harpoon.ui").toggle_quick_menu() end, { desc = "[H]arpoon [V]iew" } },
-      { "<C-6>", function() require("harpoon.ui").nav_file(1) end },
-      { "<C-5>", function() require("harpoon.ui").nav_file(2) end },
-      { "<C-4>", function() require("harpoon.ui").nav_file(3) end },
-      { "<C-3>", function() require("harpoon.ui").nav_file(4) end },
-      { "<C-2>", function() require("harpoon.ui").nav_file(5) end },
-      { "<C-1>", function() require("harpoon.ui").nav_file(6) end },
+      { "<M-6>", function() require("harpoon.ui").nav_file(1) end },
+      { "<M-5>", function() require("harpoon.ui").nav_file(2) end },
+      { "<M-4>", function() require("harpoon.ui").nav_file(3) end },
+      { "<M-3>", function() require("harpoon.ui").nav_file(4) end },
+      { "<M-2>", function() require("harpoon.ui").nav_file(5) end },
+      { "<M-1>", function() require("harpoon.ui").nav_file(6) end },
     },
   },
 
