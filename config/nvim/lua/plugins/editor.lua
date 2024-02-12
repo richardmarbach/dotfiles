d725ab1b8c2b13914ac1c01ba4829be6eaec12c6
Richard Marbach <rmarbach@gmail.com>
2024-02-12 19:11:22 +0100

    Add slimux

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -118,4 +118,29 @@ return {
       end,
     },
   },
+
+  -- Tmux integration
+  {
+    "EvWilson/slimux.nvim",
+    config = function()
+      local slimux = require("slimux")
+      slimux.setup({
+        target_socket = slimux.get_tmux_socket(),
+        target_pane = "left",
+        -- target_pane = string.format("%s.0", slimux.get_tmux_window()),
+      })
+      vim.keymap.set(
+        "v",
+        "<leader>ts",
+        ':lua require("slimux").send_highlighted_text()<CR>',
+        { desc = "Send currently highlighted text to configured tmux pane" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>ts",
+        ':lua require("slimux").send_paragraph_text()<CR>',
+        { desc = "Send paragraph under cursor to configured tmux pane" }
+      )
+    end,
+  },
 }
