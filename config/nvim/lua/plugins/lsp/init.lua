50360d22f1817db416495a854b5d9b57daf1e76c
Richard Marbach <rmarbach@gmail.com>
2024-09-17 08:17:30 +0200

    Update plugints

--- config/nvim/lua/plugins/lsp/init.lua
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -80,7 +80,7 @@ return {
             },
           },
         },
-        biome = {},
+        -- biome = {},
         typst_lsp = {},
         jsonls = {
           -- lazy-load schemastore when needed
@@ -158,6 +158,8 @@ return {
 
   {
     "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
@@ -175,7 +177,18 @@ return {
         ruby = { "rubocop" },
         python = { "black" },
       },
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+      formatters = {
+        rubocop = {
+          command = "bin/rubocop",
+        },
+      },
     },
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
   },
 
   {
