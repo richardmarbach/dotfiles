17913619a3b600c2a3ce5d3727861895657b6291
Richard Marbach <rmarbach@gmail.com>
2024-09-30 07:59:11 +0200

    Move to mise

--- config/fish/conf.d/asdf.fish
+++
@@ -1,8 +0,0 @@
-if test (uname) = "Darwin" 
-  source $HOMEBREW_PREFIX/opt/asdf/libexec/asdf.fish
-  source $HOMEBREW_PREFIX/opt/asdf/share/fish/vendor_completions.d/asdf.fish
-else 
-  source ~/.asdf/asdf.fish
-
-  mkdir -p ~/.config/fish/completions; and ln -fs ~/.asdf/completions/asdf.fish ~/.config/fish/completions
-end
