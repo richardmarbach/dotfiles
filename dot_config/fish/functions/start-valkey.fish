1a223a02c4fdf2badac81f48c8910a67b96f3fe4
Richard Marbach <rmarbach@gmail.com>
2024-12-23 16:13:41 +0100

    Add valkey and prettier

--- dot_config/fish/functions/start-valkey.fish
+++ dot_config/fish/functions/start-valkey.fish
@@ -1,3 +1,3 @@
-function start-valkey --wraps='docker container run -d --rm -it --name valkey -p "127.0.0.1:6379:6379" -d valkey/valkey:8-alpine valkey-server --appendonly yes' --description 'alias start-valkey=docker container run -d --rm -it --name valkey -p "127.0.0.1:6379:6379" -d valkey/valkey:8-alpine valkey-server --appendonly yes'
-  docker container run -d --rm -it --name valkey -p "127.0.0.1:6379:6379" -d valkey/valkey:8-alpine valkey-server --appendonly yes $argv; 
+function start-valkey --wraps='docker container run -d --rm -it --name valkey -p "127.0.0.1:6379:6379" -d valkey:8-alpine valkey --appendonly yes' --description 'alias start-valkey=docker container run -d --rm -it --name valkey -p "127.0.0.1:6379:6379" -d valkey:8-alpine valkey --appendonly yes'
+  docker container run -d --rm -it --name valkey -p "127.0.0.1:6379:6379" -d valkey:8-alpine valkey --appendonly yes $argv; 
 end
