211bf820dbd7e8652961e21aef95e4420b90e5ae
Richard Marbach <rmarbach@gmail.com>
2025-04-18 21:30:57 +0200

    Neovim as man pager

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -20,5 +20,6 @@ set -x -g RIPGREP_CONFIG_PATH ~/.config/rg/config
 set -x -g FZF_DEFAULT_COMMAND "rg --files --hidden"
 set -x -g EDITOR "nvim"
 set -x -g GEM_EDITOR "nvim"
+set -x -g MANPAGER "nvim +Man!"
 
 fish_add_path -a ~/go/bin
