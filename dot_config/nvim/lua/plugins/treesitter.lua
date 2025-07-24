b532a9aaced9ab065f9c7354dcdb007b49379a05
Richard Marbach <rmarbach@gmail.com>
2025-07-24 23:09:54 +0300

    Move by text object

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -56,57 +56,43 @@ return {
           enable = true,
           set_jumps = true, -- whether to set jumps in the jumplist
           goto_next_start = {
+            ["]b"] = "@block.outer",
             ["]m"] = "@function.outer",
-            ["]]"] = { query = "@class.outer", desc = "Next class start" },
-            --
-            -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-            ["]o"] = "@loop.*",
-            -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-            --
+            ["]["] = { query = "@class.outer", desc = "Next class start" },
+
+            ["]o"] = { query = { "@loop.inner", "@loop.outer" } },
+
             -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
             -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
             ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
             ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
           },
           goto_next_end = {
+            ["]B"] = "@block.outer",
             ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
+
+            ["]O"] = { query = { "@loop.inner", "@loop.outer" } },
+            ["]S"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+            ["]Z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
           },
           goto_previous_start = {
+            ["[b"] = "@block.outer",
             ["[m"] = "@function.outer",
             ["[["] = "@class.outer",
+
+            ["[o"] = { query = { "@loop.inner", "@loop.outer" } },
+            ["[s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+            ["[z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
           },
           goto_previous_end = {
+            ["[B"] = "@block.outer",
             ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-          },
-          -- Below will go to either the start or the end, whichever is closer.
-          -- Use if you want more granular movements
-          -- Make it even more gradual by adding multiple queries and regex.
-          goto_next = {
-            ["]d"] = "@conditional.outer",
-          },
-          goto_previous = {
-            ["[d"] = "@conditional.outer",
+
+            ["[O"] = { query = { "@loop.inner", "@loop.outer" } },
+            ["[S"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+            ["[Z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
           },
         },
-        --   select = {
-        --     enable = true,
-        --     lookahead = true,
-        --     keymaps = {
-        --       ["af"] = "@function.outer",
-        --       ["if"] = "@function.inner",
-        --       ["ac"] = "@class.outer",
-        --       ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-        --       ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-        --     },
-        --     selection_modes = {
-        --       ["@parameter.outer"] = "v", -- charwise
-        --       ["@function.outer"] = "V", -- linewise
-        --       ["@class.outer"] = "<c-v>", -- blockwise
-        --     },
-        --     include_surrounding_whitespace = true,
-        --   },
       },
     },
     config = function(_, opts)
@@ -127,5 +113,5 @@ return {
     config = true,
   },
 
-  -- { "nvim-treesitter/nvim-treesitter-textobjects" },
+  { "nvim-treesitter/nvim-treesitter-textobjects" },
 }
