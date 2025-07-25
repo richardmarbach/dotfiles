ba73c9eeeb4cd9f90966b33cfc1c88f91da2fc09
Richard Marbach <rmarbach@gmail.com>
2025-07-25 21:54:16 +0300

    Use UFO for better folds

--- dot_config/nvim/lua/config/settings.lua
+++ dot_config/nvim/lua/config/settings.lua
@@ -71,18 +71,18 @@ vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
 vim.o.foldenable = true
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
-vim.o.foldmethod = "expr"
-vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.o.foldtext = ""
+-- vim.o.foldmethod = "expr"
+-- vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- vim.o.foldtext = ""
 vim.o.foldcolumn = "0"
 vim.o.foldnestmax = 4
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client:supports_method("textDocument/foldingRange") then
-      local win = vim.api.nvim_get_current_win()
-      vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   callback = function(args)
+--     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     if client:supports_method("textDocument/foldingRange") then
+--       local win = vim.api.nvim_get_current_win()
+--       vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+--     end
+--   end,
+-- })
