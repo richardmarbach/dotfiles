abde431dc9c4941a67124682db9dd7b6206f30b5
Richard Marbach <rmarbach@gmail.com>
2026-03-31 12:25:32 +0200

    Fix syntax highlighting

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -41,10 +41,7 @@ return {
       -- Enable treesitter highlighting for all installed languages
       vim.api.nvim_create_autocmd("FileType", {
         callback = function(args)
-          local ok = pcall(vim.treesitter.start, args.buf)
-          if ok then
-            vim.bo[args.buf].syntax = ""
-          end
+          pcall(vim.treesitter.start, args.buf)
         end,
       })
 
