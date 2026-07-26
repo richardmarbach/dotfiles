e69d0d9101c0a22d196baf85eb26aa6f03de1737
Richard Marbach <rmarbach@gmail.com>
2026-07-26 22:34:21 +0200

    Make diffs more readable

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,14 @@
 vim.pack.add({ "https://github.com/ellisonleao/gruvbox.nvim" })
 
 vim.o.termguicolors = true
-require("gruvbox").setup({ contrast = "soft" })
+require("gruvbox").setup({
+  contrast = "soft",
+  overrides = {
+    -- Dim full-line diff backgrounds; keep changed text (DiffText) prominent.
+    DiffAdd = { bg = "#26332b" },
+    DiffChange = { bg = "#2b2a22" },
+    DiffDelete = { bg = "#3a2626" },
+    DiffText = { bg = "#4f6c3f", bold = true },
+  },
+})
 vim.cmd([[colorscheme gruvbox]])
